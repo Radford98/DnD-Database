112cb69af522672005c79a1e012a267cbff5f576
Brad <powelbra@oregonstate.edu>
2019-03-05 20:01:28 -0800

    Update/Delete
    Added update and delete buttons (with function) to every page.

--- app/files/deleteRow.js
+++ app/files/deleteRow.js
@@ -1,3 +1,5 @@
+/* This function takes table being edited (like 'Specials') and the id of the row to be deleted.
+It makes an ajax call to delete the table and reload the page on success.   */
 function deleteRow(table, id) {
     var XHR = new XMLHttpRequest();
     var url = '/manage' + table + '/' + id;

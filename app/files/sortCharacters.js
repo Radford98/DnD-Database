f97236570751a0ae3e3daf689dfec8ca89113eca
Brad <powelbra@oregonstate.edu>
2019-03-13 21:23:40 -0700

    Started on character sort

---
+++ app/files/sortCharacters.js
@@ -0,0 +1,16 @@
+/* When the sort-select (#sort) changes, it hides all the options (in the case the user changes between them),
+then constructs a selector using #sort's value and shows only that one.
+*/
+$('#sort').change(function (event) {
+    $('.sortSelect').hide();
+    $('#sort' + $(this).val()).show();
+});
+
+$('#sortPlayer').change(function (event) {
+    if ($(this).val() != '') {
+        $.ajax({
+            url: '/manageCharacters/player/' + $(this).val(),
+            type: 'GET'
+        })
+    }
+});
